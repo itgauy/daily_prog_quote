8d49d03547393cbf7012c9ff9daa5ddf6a26635a
itgauy <torreslian@duck.com>
2025-04-05 21:17:32 +0800

    first commit

---
+++ source_code_pls.py
@@ -0,0 +1,19 @@
+# Author: Lian T. (itgauy)
+# Date: 2024-04-05
+
+x = "I can fix the world"
+y = "but they won't give me the source code"
+z = "\n"
+i = 0
+
+def fix_the_world():
+    return x + ",\n" + y + "."
+
+def style():
+  while i < 5:
+      print(z)
+      i += 1
+
+style()
+print(fix_the_world())
+style()
