bba300589326cadc3057261aee71868e2656db44
itgauy <torreslian@duck.com>
2025-04-07 18:33:27 +0800

    Refactor: Improve code formatting and structure

--- source_code_pls.py
+++ source_code_pls.py
@@ -1,18 +1,21 @@
 # Author: Lian T. (itgauy)
 # Date: 2025-04-05
 
+# Quote: I can fix the world, but they won't give me the source code.
+
 x = "I can fix the world"
 y = "but they won't give me the source code"
 z = "\n"
-i = 0
+
 
 def fix_the_world():
-    return x + ",\n" + y + "."
+  return x + ",\n" + y + "."
 
 def style():
+  i = 0
   while i < 5:
-      print(z)
-      i += 1
+    print(z)
+    i += 1
 
 style()
 print(fix_the_world())
