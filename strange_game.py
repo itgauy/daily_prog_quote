d3299655015b41ae46b48d90e8d053936615a32c
itgauy <torreslian@duck.com>
2025-04-07 18:41:10 +0800

    Quote Added for Tiktok

--- strange_game.py
+++ strange_game.py
@@ -1,6 +1,8 @@
 # Author: Lian T. (itgauy)
 # Date: 2025-04-06
 
+# Quote: A strange game. The only winning move is not to play.
+
 a = "The only winning move is"
 s = "not to play"
 c = "."
@@ -9,8 +11,8 @@ ii = "\n"
 def void():
   i = 0
   while i < 5:
-      print(ii)
-      i += 1
+    print(ii)
+    i += 1
 
 secret = [115, 116, 114, 97, 110, 103, 101, 32, 103, 97, 109, 101]
 o = ''.join(chr(num) for num in secret)
